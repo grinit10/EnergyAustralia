c792d4322c956d2deede089e27f9365f39caa4a1
Arnab <grinit10@gmail.com>
2019-03-08 05:43:11 +1100

    Adding a fresh react app

--- clientApp/src/index.js
+++ clientApp/src/index.js
@@ -4,16 +4,11 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
-import { createBrowserHistory } from 'history';
-// import configureStore from './store/configureStore';
+import {BrowserRouter} from 'react-router-dom';
 import configureStore from './redux/store';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-// Create browser history to use in the Redux store
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const history = createBrowserHistory({ basename: baseUrl });
 
 // Get the application-wide store instance, prepopulating with state from the server where available.
 const store = configureStore()
@@ -22,9 +17,9 @@ const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
+    <BrowserRouter>
       <App />
-    </ConnectedRouter>
+    </BrowserRouter>
   </Provider>,
   rootElement);
 
