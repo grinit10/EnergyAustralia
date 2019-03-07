2acd7858f86cafdf5a68a70a504e88a950c5dba6
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2019-03-07 17:15:03 +0530

    Adding UT.

---
+++ UT/Services/TestGetCarShows.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture;
+using Domains.Enums;
+using Domains.ViewModels;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT.Services
+{
+    public class TestGetCarShows: CarShowServicetestConfig
+    {
+        [SetUp]
+        public void Setup()
+        {
+            base.SetUp();
+            
+        }
+
+        [Test]
+        public async Task ReturnEmptyWhenNoDataReturnedFromApiAsync()
+        {
+            mockCarshowFacede.Setup(p => p.GetAllResponseAsync(It.IsAny<EntityTypes>()))
+                .ReturnsAsync(() => new List<CarShow>());
+            var result = await SystemUnderTest.GetCarShows();
+
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [Test]
+        public async Task ReturnNonEmptyWhenSomeDataReturnedFromApiAsync()
+        {
+            mockCarshowFacede.Setup(p => p.GetAllResponseAsync(It.IsAny<EntityTypes>()))
+                .ReturnsAsync(() => TestCarShows);
+            var result = await SystemUnderTest.GetCarShows();
+
+            Assert.Greater(result.Count, 0);
+        }
+    }
+}
