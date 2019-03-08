135318bfeff5098c4fb5d846fabd859cc7be8d8e
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2019-03-08 17:49:05 +0530

    Adding spanshot testing

--- clientApp/src/App.test.js
+++ clientApp/src/App.test.js
@@ -1,15 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+test('spanshot test ', () => {
+  const component = renderer.create(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>)
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 })
